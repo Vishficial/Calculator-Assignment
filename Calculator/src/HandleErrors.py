ada8a19bfdd0936b780ad30fd39188cd7b7fecae
Vishwjeet Singh <vishwjeet06062003@gmail.com>
2024-07-12 18:09:23 +0530

    Updates for handling the errors of input

---
+++ Calculator/src/HandleErrors.py
@@ -0,0 +1,46 @@
+#To get the integer value and preventing error if anyother value is entered
+
+
+#Returns an integer value or prints "wrong input" for any other input
+
+#For the first Number
+def Num1():
+    ash = True
+    while ash is True:
+
+        Entry = input("Enter the First Number:")
+        try:
+            First = int(Entry)
+            ash = False
+            return First
+
+        except ValueError:
+            print("Wrong input! Try Again")
+
+#For the second Number
+def Num2():
+    ash = True
+    while ash is True:
+
+        Entry = input("Enter the Second Number:")
+        try:
+            Sec = int(Entry)
+            ash = False
+            return Sec
+
+        except ValueError:
+            print("wrong input! Try Again")
+
+#For the Operator
+def Opp():
+    Harry = True
+    while Harry is True:
+
+        Entry = input("Choose the Operator from Menu:")
+        try:
+            opp = int(Entry)
+            ash = False
+            return opp
+        except ValueError:
+            print("Wrong input! Try Again")
+
